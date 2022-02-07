920414c14082938a2121ebd533171b495e3bb249
El Moctar Ebnou <elmoctarebnou@Els-MacBook-Pro.local>
2022-02-06 22:02:51 -0700

    New folder script

---
+++ new.sh
@@ -0,0 +1,58 @@
+<<comment
+    Util shell script for creating component and domain.
+    ARG1 is the directory(components or domains)
+    ARG2 is the name of the component
+comment
+
+ARG1=$1
+ARG2=$2
+FOLDER=''
+
+
+function create_folder(){
+
+    COMPONENT_NAME=${ARG2}
+
+    mkdir ./src/${FOLDER}/${COMPONENT_NAME}
+
+    cat > ./src/${FOLDER}/${COMPONENT_NAME}/${COMPONENT_NAME}.js <<EOM
+import React, { useState, useEffect } from 'react';
+
+import styles from './${COMPONENT_NAME}.style.js';
+
+const ${COMPONENT_NAME} = (props) => {
+
+    return (
+        <div>
+            ${COMPONENT_NAME}
+        </div>
+    )
+};
+
+export default ${COMPONENT_NAME};
+EOM
+
+    cat > ./src/${FOLDER}/${COMPONENT_NAME}/${COMPONENT_NAME}.style.js <<EOM
+export default function styles(){
+    return {
+
+    }
+};
+EOM
+
+    echo '------------'
+    echo Folder ${ARG2} added to ./src/${FOLDER}/${COMPONENT_NAME}
+    echo '------------'
+}
+
+
+if [ ${ARG1} == 'component' ]
+    then
+        FOLDER='components'
+        create_folder
+    fi
+if [ ${ARG1} == 'domain' ]
+    then
+        FOLDER='domains'
+        create_folder
+    fi
